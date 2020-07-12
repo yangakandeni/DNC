69ff57be6d82752791d1482525901c3cd7a429db
yangakandeni <kandeni.yanga@gmail.com>
2020-07-12 17:18:16 +0200

    created a 1-Many Rel btwn phonebook and leads

--- phonebook/admin.py
+++ phonebook/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Lead, Selection, CallDetailRecord, Phonebook
 
-# admin.site.register(Lead)
-# admin.site.register(Selection)
-# admin.site.register(CallDetailRecord)
-# admin.site.register(Phonebook)
+admin.site.register(Lead)
+admin.site.register(Selection)
+admin.site.register(CallDetailRecord)
+admin.site.register(Phonebook)
--- phonebook/views.py
+++ phonebook/views.py
@@ -3,39 +3,37 @@ import re
 from django.shortcuts import render
 from .models import Phonebook, Lead, Selection, CallDetailRecord
 
-
 def index(request):
-    # # tem manual phonebook upload
-    # upload = input("Upload Phonebook: ")
+    # tem manual phonebook upload
+    upload = input("Upload Phonebook: ")
     
     # if there is no data in the CDR, populate relevant tables
     cdr_data = CallDetailRecord.objects.all()
     if len(cdr_data) <= 0:
-        pass
         # create phonebook
-        # phonebook = Phonebook()
-        # phonebook.name = upload.split('.')[0]
-        # phonebook.save()
+        phonebook = Phonebook()
+        phonebook.name = upload.split('.')[0]
+        phonebook.save()
         
-        # # create lead
-        # with open(f'static/phonebooks/{upload}') as inFile:
-        #     reader = csv.reader(inFile)
+        # create lead
+        with open(f'static/phonebooks/{upload}') as inFile:
+            reader = csv.reader(inFile)
             
-        #     line_count = 0
-        #     for row in reader:
-        #         if line_count == 0:
-        #             file_headers = [title.strip().lower() for title in row if len(title) > 0]
-        #             name_index = file_headers.index('contact name')
-        #             number_index = file_headers.index('contact number')
-        #             age_index = file_headers.index('age')                    
-        #             line_count += 1
-        #         else:
-        #             lead = Lead()
-        #             lead.contact_name = row[name_index]
-        #             lead.contact_number = row[number_index]
-        #             lead.age = row[age_index]
-        #             lead.save()
-        #     lead.phonebook.add(phonebook)
+            line_count = 0
+            for row in reader:
+                if line_count == 0:
+                    file_headers = [title.strip().lower() for title in row if len(title) > 0]
+                    name_index = file_headers.index('contact name')
+                    number_index = file_headers.index('contact number')
+                    age_index = file_headers.index('age')                    
+                    line_count += 1
+                else:
+                    lead = Lead()
+                    lead.contact_name = row[name_index]
+                    lead.contact_number = row[number_index]
+                    lead.age = row[age_index]
+                    lead.phonebook = phonebook
+                    lead.save()
                 
     context = {
         # 'phonebook': new_phonebook
