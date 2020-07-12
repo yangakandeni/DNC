5459b0f335155aba870970081d064a392394ed11
yangakandeni <kandeni.yanga@gmail.com>
2020-07-12 12:21:26 +0200

    added str method to models

--- phonebook/admin.py
+++ phonebook/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Lead, Selection, CallDetailRecord, Phonebook
 
-# Register your models here.
+admin.site.register(Lead)
+admin.site.register(Selection)
+admin.site.register(CallDetailRecord)
+admin.site.register(Phonebook)
--- phonebook/models.py
+++ phonebook/models.py
@@ -1,21 +1,37 @@
 from django.db import models
 
-class Phonebook(models.Model):
-    name = models.CharField(max_length=30)
-    leads = models.ManyToManyField(Lead)
-    date = models.DateField(auto_now=False, auto_now_add=False)
-
 class Lead(models.Model):
     contact_name = models.CharField(max_length=30)
     contact_number = models.CharField(max_length=30)
     age = models.CharField(max_length=3)
+    
+    def __str__(self):
+        return f'{self.contact_name} | {self.contact_number}'
+    
+
+class Phonebook(models.Model):
+    name = models.CharField(max_length=30)
+    leads = models.ManyToManyField(Lead)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Selection(models.Model):
+    key = models.CharField(max_length=2)
+    description = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f'{self.name} | {self.description}'
+    
 
 class CallDetailRecord(models.Model):
     lead = models.ForeignKey(Lead, on_delete=models.CASCADE)
     selection = models.ForeignKey(Selection, on_delete=models.CASCADE) 
     date = models.DateField(auto_now=False, auto_now_add=False)
     
-class Selection(models.Model):
-    key = models.CharField(max_length=2)
-    description = models.CharField(max_length=30)
+    def __str__(self):
+        return f'{self.date} | {self.lead.contact_name} | {self.lead.contact_number} | {self.selection.key}'
+    
 
