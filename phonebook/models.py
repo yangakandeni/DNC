3e755cea08aa8a96370b2b594665f16e57ac7f0a
yangakandeni <kandeni.yanga@gmail.com>
2020-07-12 14:00:43 +0200

    created index.html

--- phonebook/models.py
+++ phonebook/models.py
@@ -23,7 +23,7 @@ class Selection(models.Model):
     description = models.CharField(max_length=30)
     
     def __str__(self):
-        return f'{self.name} | {self.description}'
+        return f'{self.key} | {self.description}'
     
 
 class CallDetailRecord(models.Model):
