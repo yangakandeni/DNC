d036544c7fecd186e3b7cbdd361c213680fea62e
yangakandeni <kandeni.yanga@gmail.com>
2020-07-12 11:45:45 +0200

    created models

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Phonebook(models.Model):
+    name = models.CharField(max_length=30)
+    leads = models.ManyToManyField(Lead)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+
+class Lead(models.Model):
+    contact_name = models.CharField(max_length=30)
+    contact_number = models.CharField(max_length=30)
+    age = models.CharField(max_length=3)
+
+class CallDetailRecord(models.Model):
+    lead = models.ForeignKey(Lead, on_delete=models.CASCADE)
+    selection = models.ForeignKey(Selection, on_delete=models.CASCADE) 
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    
+class Selection(models.Model):
+    key = models.CharField(max_length=2)
+    description = models.CharField(max_length=30)
+
