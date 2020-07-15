11b89b4490b9685dc9a898d0fec464a34e8f8907
yangakandeni <kandeni.yanga@gmail.com>
2020-07-15 16:06:22 +0200

    can generate leads from csv or dict using 1 function

--- phonebook/views.py
+++ phonebook/views.py
@@ -12,33 +12,38 @@ def index(request):
     csv_file = 'phonebook1.csv'
 
     # get all records in the CDR and covert to dict
-    cdr_records = convert_to_dict()
+    cdr_dict = convert_to_dict(model=CallDetailRecord)
 
-    if len(cdr_records) <= 0:
+    if len(cdr_dict) <= 0:
         # generate leads using phonebook data
-        create_leads(csv_file)
+        create_lead(csv_file)
 
         # simulate CDR
         simulate_cdr()
+        
+        # update DNCs
+        update_dnc()    
     else:       
-        # update DNCs 
-        update_dnc('3')
+        # get all records in the DNC and covert to dict 
+        dnc_dict = convert_to_dict(model=DoNotCall)
         
         # convert phonebook data to dict
         phonebook_dict = convert_to_dict(csvfilepath=f'static/phonebooks/{csv_file}', contact_name='contact name', contact_number='contact number', age='age')
         
-        # for num in phonebook_dict:
-        #     if num in cdr_records:
-        #         if cdr_records[num] == '3':
-        #             # save to DNC table
-        #             dnc = DoNotCall()
-        #             dnc.lead = Lead.objects.filter(contact_number='3')
-        #             dnc.save()
-                    
-        #         else:
-        #             print(f'\n{num}')
-        
-        return HttpResponse(json.dumps(cdr_records))
+        for num in phonebook_dict:
+            if not num in dnc_dict:
+                # print(f'\n{num}')
+                # create lead 
+                create_lead(csvfile=csv_file,_dict=phonebook_dict, key=num, subkey_age='age', subkey_contact_name='name', subkey_contact_number='number', type='dict')
+                # # update DNCs
+                # update_dnc()
+            else:
+                print(num)
+                
+        # return HttpResponse(json.dumps(dnc_dict))
+    # simulate CDR
+    simulate_cdr()
+    
 
     # parse mock CDR into template
     context = {
@@ -348,7 +353,7 @@ def index(request):
     # #     return render(request, 'phonebook/index.htm')
 
 
-def convert_to_dict(csvfilepath=None, contact_name=None, contact_number=None, age=None):
+def convert_to_dict(model=None, csvfilepath=None, contact_name=None, contact_number=None, age=None):
 
     # dict storage
     dict_storage = dict()
@@ -374,46 +379,60 @@ def convert_to_dict(csvfilepath=None, contact_name=None, contact_number=None, ag
     
     else:
         # convert CDR entries to dictionary
-        for record in CallDetailRecord.objects.all():
-            dict_storage[record.lead.contact_number] = dict_storage.setdefault(record.lead.contact_number, {
-                'name': record.lead.contact_name,
-                'age' : record.lead.age,
-                'selection': record.selection.key
-            })
-
+        for record in model.objects.all():
+            if model is CallDetailRecord:
+                dict_storage[record.lead.contact_number] = dict_storage.setdefault(record.lead.contact_number, {
+                    'name': record.lead.contact_name,
+                    'age' : record.lead.age,
+                    'selection': record.selection.key
+                })
+            elif model is DoNotCall:
+                dict_storage[record.lead.contact_number] = dict_storage.setdefault(record.lead.contact_number, {
+                    'name': record.lead.contact_name,
+                    'age' : record.lead.age
+                })
 
     return dict_storage
 
 # print(convert_to_dict(csvfilepath='static/phonebooks/phonebook1.csv', contact_name='contact name', contact_number='contact number', age='age'))
 
-def create_leads(csvfile):
+def create_lead(csvfile=None, _dict=None, key=None, subkey_age=None, subkey_contact_name=None, subkey_contact_number=None, type='csv'):
 
     # make instance of Phonebook
     phonebook = Phonebook()
     phonebook.name = csvfile.split('.')[0]
     phonebook.save()
 
-    # Create new lead
-    with open(f'static/phonebooks/{csvfile}') as inFile:
-        reader = csv.reader(inFile)
+    # Create new lead FROM CSVFILE
+    if type is 'csv':
+        with open(f'static/phonebooks/{csvfile}') as inFile:
+            reader = csv.reader(inFile)
 
-        line_count = 0
-        for row in reader:
-            if line_count == 0:
-                file_headers = [title.strip().lower() for title in row if len(title) > 0]
-                name_index = file_headers.index('contact name')
-                number_index = file_headers.index('contact number')
-                age_index = file_headers.index('age')                    
-                line_count += 1
-            else:
-                # create lead
-                lead = Lead()
-                lead.contact_name = row[name_index]
-                lead.contact_number = row[number_index]
-                lead.age = row[age_index]
-                lead.phonebook = phonebook
-                lead.save()
-
+            line_count = 0
+            for row in reader:
+                if line_count == 0:
+                    file_headers = [title.strip().lower() for title in row if len(title) > 0]
+                    name_index = file_headers.index('contact name')
+                    number_index = file_headers.index('contact number')
+                    age_index = file_headers.index('age')                    
+                    line_count += 1
+                else:
+                    # create lead
+                    lead = Lead()
+                    lead.contact_name = row[name_index]
+                    lead.contact_number = row[number_index]
+                    lead.age = row[age_index]
+                    lead.phonebook = phonebook
+                    lead.save()
+    else:
+        # create new lead FROM DICTIONARY
+        lead = Lead()
+        lead.age = _dict[key].get(subkey_age)
+        lead.contact_name = _dict[key].get(subkey_contact_name)
+        lead.contact_number = _dict[key].get(subkey_contact_number)
+        lead.phonebook = phonebook
+        lead.save()
+        
     return None
 
 def upload_phonebook():
@@ -445,11 +464,11 @@ def simulate_cdr():
             
     return None
 
-def update_dnc(dnc_key):
+def update_dnc(dnc_key='4'):
     
-    dnc_records = CallDetailRecord.objects.filter(selection__key=dnc_key)
+    dnc_dict = CallDetailRecord.objects.filter(selection__key=dnc_key)
     
-    for record in dnc_records:
+    for record in dnc_dict:
         dnc_list = DoNotCall()
         dnc_list.lead = record.lead
         dnc_list.save()
